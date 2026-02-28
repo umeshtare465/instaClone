3a8aaf0c13375d6159ef165cd7b92a3d4f79c882
student umeshtare465 <umeshtare465@gmail.com>
2026-03-01 02:42:16 +0530

    like clear

--- Backend/src/controllers/post.controllers.js
+++ Backend/src/controllers/post.controllers.js
@@ -75,7 +75,22 @@ async function likePostController(req, res) {
 }
 
 async function getFeedController(req, res) {
-  const posts = await postModel.find().populate("user");
+  const user = req.user;
+  const posts = await Promise.all(
+    (await postModel.find().populate("user").lean()).map(async (post) => {
+      /**
+       * type of post is mongoose object
+       * thats why we use lean
+       */
+      const isLiked = await likeModel.findOne({
+        user: user.username,
+        post: post._id,
+      });
+
+      post.isLiked = Boolean(isLiked);
+      return post;
+    }),
+  );
 
   res.status(200).json({
     message: "getted feed successfully",
--- node_modules/undefined/instaclone/src/features/post/components/Post.jsx
+++ node_modules/undefined/instaclone/src/features/post/components/Post.jsx
@@ -14,6 +14,7 @@ const Post = ({ user, post }) => {
         <div className="left">
           <button>
             <svg
+              className={post.isLiked ? "Like" : ""}
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="currentColor"
